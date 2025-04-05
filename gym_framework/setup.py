b2903d35238dfcfcd137b704b6386e1c508f7c0f
Gerardo Mikael Do Carmo Pereira <gerardomikael1234@gmail.com>
2025-04-04 22:08:52 -0300

    Adicionar setup.py para preparar o framework como biblioteca

---
+++ gym_framework/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gym_framework',
+    version='0.1',
+    packages=find_packages(),  # detect subdependencies
+    install_requires=[],
+    author='Mikael, Yoni e George',
+    description='Uma biblioteca voltada para frameworks com ferramentas para extração, transformação e modelagem de dados',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7', #Versão minima do python que aceitas dataclasses
+)
